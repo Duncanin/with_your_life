d22a8732ed32cc9ac64ce461f6172a4c9fd69bd3
Katie <tsaihsuan429@gmail.com>
2025-08-25 13:39:40 +0800

    新增商品列表 section-1

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import "./assets/scss/all.scss";
 import "./node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+
+document.addEventListener('click', function(e) {
+  if (e.target.closest('.icon-btn[aria-label="加入收藏"]')) {
+    const icon = e.target.closest('button').querySelector('i');
+    icon.classList.toggle('bi-heart');
+    icon.classList.toggle('bi-heart-fill');
+    icon.classList.toggle('text-danger');
+  }
+});
